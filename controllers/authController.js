745ea9cf5039a0c3b5d9aad91d98e264a071a86f
Arek <arek93r@gmail.com>
2025-04-04 19:22:30 +0200

    secure product routes with JWT and admin

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,29 +2,28 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-//rejestracja
+//register
 exports.register = async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { email, password, role } = req.body;
     const hash = await bcrypt.hash(password, 10);
-    const user = new User({ email, password: hash });
+    const user = new User({ email, password: hash, role });
     await user.save();
-    res.status(201).json({ message: "Użytkownik utworzony" });
+    res.status(201).json({ message: "User registered success" });
   } catch (err) {
-    res.status(400).json({ message: "Błąd podczas rejestracji" });
+    res.status(400).json({ message: "Regist failed" });
   }
 };
 
-// logowanie
+//login
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-    if (!user)
-      return res.status(404).json({ message: "Nie znaleziono użytkownika" });
+    if (!user) return res.status(404).json({ message: "user not found" });
 
     const match = await bcrypt.compare(password, user.password);
-    if (!match) return res.status(401).json({ message: "Nieprawidłowe hasło" });
+    if (!match) return res.status(401).json({ message: "incorrect pasword" });
 
     const token = jwt.sign(
       { id: user._id, role: user.role },
@@ -34,6 +33,6 @@ exports.login = async (req, res) => {
 
     res.json({ token });
   } catch (err) {
-    res.status(500).json({ message: "Błąd logowania" });
+    res.status(500).json({ message: "login err" });
   }
 };
