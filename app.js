7af2ef96cdd0ae89e9748867b351db437cc36b6c
Arek <arek93r@gmail.com>
2025-04-04 14:41:24 +0200

    connect to MongoDB and setup environment variables

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+require("dotenv").config()
 const express = require("express")
+const mongoose = require("mongoose")
+
+
+
 const app = express()
 app.use(express.json())
 
-app.get("/", (req, res) => res.send("Api działa"))
-
-
-app.listen(5000, () => console.log("Serwer działa na porcie 5000"))
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB connected")
+    app.listen(process.env.PORT, () =>
+    console.log(`Server running on port ${process.env.PORT}`)
+)
+})
+.catch(err => console.error(err))
